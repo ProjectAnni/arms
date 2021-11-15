3253eb7b916ed36b7a1844ff0dd1fd178597b18d
Yesterday17 <mmf@mmf.moe>
2021-11-15 21:50:19 +0800

    editor onChange, fix duplicated wasm load

--- packages/renderer/src/components/ArmsEditor.tsx
+++ packages/renderer/src/components/ArmsEditor.tsx
@@ -1,4 +1,4 @@
-import {defineComponent, onMounted, ref} from 'vue';
+import {defineComponent, onMounted, onUnmounted, ref} from 'vue';
 import * as monaco from 'monaco-editor';
 import {wireTmGrammars} from 'monaco-editor-textmate';
 import {Registry} from 'monaco-textmate';
@@ -10,12 +10,19 @@ import githubTheme from '/@/monaco/github.theme';
 
 export default defineComponent({
   name: 'ArmsEditor',
-  setup() {
-    const editor = ref();
+  emits: ['onChange'],
+  setup(_, ctx) {
+    const editor = ref<monaco.editor.IStandaloneCodeEditor>();
+    const div = ref<HTMLElement>();
     const anni = useAnni();
+    const data = ref('');
 
     onMounted(async () => {
-      await loadWASM(anni.onigasmUrl);
+      try {
+        await loadWASM(anni.onigasmUrl);
+        // eslint-disable-next-line no-empty
+      } catch {
+      }
 
       const registry = new Registry({
         getGrammarDefinition: async () => {
@@ -26,16 +33,36 @@ export default defineComponent({
         },
       });
       const grammars: Map<string, string> = new Map([['toml', 'source.toml']]);
-      monaco.editor.defineTheme('vs-code-theme-converted', {base: 'vs', ...githubTheme});
+      monaco.editor.defineTheme('theme-github', {base: 'vs', ...githubTheme});
 
-      editor.value = await monaco.editor.create(editor.value, {language: 'toml', theme: 'vs-code-theme-converted'});
+      editor.value = await monaco.editor.create(div.value as HTMLElement, {
+        language: 'toml',
+        theme: 'theme-github',
+        automaticLayout: true,
+        codeLens: false,
+        value: data.value,
+      });
       monaco.languages.register({id: 'toml'});
       await wireTmGrammars(monaco, registry, grammars, editor.value);
+      div.value = editor.value.getDomNode() as HTMLElement;
+
+      // listen change event
+      editor.value?.onDidChangeModelContent(() => {
+        const value = editor.value?.getValue() || '';
+        if (data.value !== value) {
+          data.value = editor.value?.getValue() || '';
+          ctx.emit('onChange', data.value);
+        }
+      });
+    });
+
+    onUnmounted(() => {
+      editor.value?.dispose();
     });
 
     const style = {width: '100%', height: '100%'};
     return () => (
-      <div ref={editor} class="monaco-editor-arms" style={style}/>
+      <div ref={div} class="monaco-editor-arms" style={style}/>
     );
   },
 });
